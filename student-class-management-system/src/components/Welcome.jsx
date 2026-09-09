d265487b53009e3ebe7b80d8b65ed7bee3d076ba
Poojadk0316 <poojadk1613@gmail.com>
2026-09-09 09:26:52 +0530

    first step towards the student task management system

---
+++ student-class-management-system/src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to the Student\ task Management</h1>
+      <p>Please navigate through the menu to manage students and classes.</p>
+    </div>
+  );
+}
+export default Welcome; 
